512c78f8aa1509737e9a9118bb24e79f8d0d0fb7
VM Marco <59030832+Marco-v-BaldanII@users.noreply.github.com>
2024-10-10 15:02:53 +0200

    initial commit

---
+++ MyGameMaker/MyGameEngine/GameObject.cpp
@@ -0,0 +1,9 @@
+#include "GameObject.h"
+#include <iostream>
+
+using namespace std;
+
+void GameObject::paint() {
+	
+	cout << "random message";
+}
---
+++ MyGameMaker/MyGameEngine/GameObject.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/* don't include external dependencies on the .h, do so in the cpp */
+
+
+
+
+
+class GameObject
+{
+
+public :
+
+	void paint();
+
+};
+

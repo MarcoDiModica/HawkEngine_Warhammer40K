d0b6ec18eeff0927dc68b75894052dbe154153f0
Peluchomp <pau.mora.sole@gmail.com>
2024-10-18 15:32:53 +0200

    Started doing the camera i'll finish later

--- MyGameMaker/MyGameEditor/main.cpp
+++ MyGameMaker/MyGameEditor/main.cpp
@@ -11,6 +11,8 @@
 #include "assimp/scene.h" 
 #include "assimp/postprocess.h"
 #include "../MyGameEngine/Mesh.h"
+#include "Camera.h"
+#include "imgui.h"
 
 using namespace std;
 
@@ -20,6 +22,7 @@ using ivec2 = glm::ivec2;
 using vec3 = glm::dvec3;
 Mesh mesh;
 
+static Camera camera;
 static const ivec2 WINDOW_SIZE(1280, 720);
 static const auto FPS = 60;
 static const auto FRAME_DT = 1.0s / FPS;
@@ -46,11 +49,38 @@ static void draw_triangle(const u8vec4& color, const vec3& center, double size) 
 	glEnd();
 }
 
+static void drawFloorGrid(int size, double step) {
+	glColor3ub(0, 0, 0);
+	glBegin(GL_LINES);
+	for (double i = -size; i <= size; i += step) {
+		glVertex3d(i, 0, -size);
+		glVertex3d(i, 0, size);
+		glVertex3d(-size, 0, i);
+		glVertex3d(size, 0, i);
+	}
+	glEnd();
+}
+void move_camera() 
+{
+	//move the transform of the camera
+    if (ImGui::IsKeyDown(ImGuiKey_W)) camera.transform().translate(camera.transform().fwd() * 0.1);
+	if (ImGui::IsKeyDown(ImGuiKey_S)) camera.transform().translate(-camera.transform().fwd() * 0.1);
+	if (ImGui::IsKeyDown(ImGuiKey_A)) camera.transform().translate(-camera.transform().left() * 0.1);
+	if (ImGui::IsKeyDown(ImGuiKey_D)) camera.transform().translate(camera.transform().left() * 0.1);
+	if (ImGui::IsKeyDown(ImGuiKey_Q)) camera.transform().translate(-camera.transform().up() * 0.1);
+	if (ImGui::IsKeyDown(ImGuiKey_E)) camera.transform().translate(camera.transform().up() * 0.1);
+	//rotate the transform of the camera
+	
+	
+	
+	
 
+}
 
 static void display_func() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
+	glLoadMatrixd(&camera.view()[0][0]);
+	drawFloorGrid(16, 0.25);
 	mesh.Draw();
 }
 
@@ -59,7 +89,8 @@ int main(int argc, char** argv) {
 	MyGUI gui(window.windowPtr(), window.contextPtr());
 
 	init_openGL();
-
+	camera.transform().pos() = vec3(0, 1, 4);
+	camera.transform().rotate(glm::radians(180.0), vec3(0, 1, 0));
 	
 	mesh.LoadMesh("BakerHouse.fbx");
 
@@ -67,6 +98,7 @@ int main(int argc, char** argv) {
 		const auto t0 = hrclock::now();
 		display_func();
 		gui.render();
+		move_camera();
 		window.swapBuffers();
 		const auto t1 = hrclock::now();
 		const auto dt = t1 - t0;

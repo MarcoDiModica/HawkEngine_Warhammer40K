11dcb8c15afa4da15a1ce803df3bbb1d728d5e6d
Jordopol <jordi.tur@estudiantat.upc.edu>
2024-10-27 21:20:07 +0100

    UI BaseClass

---
+++ MyGameMaker/MyGameEditor/UIElement.h
@@ -0,0 +1,40 @@
+#ifndef __UIELEMENT_H__
+#define __UIELEMENT_H__
+#pragma once
+
+#include <string>
+
+enum class UIType
+{
+	SETTINGS,
+	DEFAULT,
+	UNKNOWN	
+};
+
+class UIElement
+{
+public:
+
+	UIElement(UIType, std::string name);
+	virtual ~UIElement();
+
+	virtual bool Draw() = 0;
+
+	virtual UIType GetType() const;
+	virtual std::string GetName() const;
+
+	virtual bool GetState();
+	virtual void SetState(bool state);
+	virtual void SwitchState();
+
+protected:
+
+	UIType type = UIType::UNKNOWN;
+	std::string name;
+	bool enabled = false;
+
+};
+
+#endif // !__UIELEMENT_H__
+
+

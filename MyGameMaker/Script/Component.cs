c5b24ce0418cbe3b8eb1b003e70b425f0b49b4a5
Jordopol <jordi.tur@estudiantat.upc.edu>
2025-01-17 21:57:56 +0100

    Extended Component.cs

--- MyGameMaker/Script/Component.cs
+++ MyGameMaker/Script/Component.cs
@@ -1,20 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HawkEngine
 {
-    public class Component
+    public abstract class Component
     {
-        UIntPtr CplusplusInstance;
+        protected UIntPtr CplusplusInstance;
 
-        public Component() { EngineCalls.print("component in C#"); }
+        public string name { get; private set; }
+        public bool enabled { get; private set; }
+
+        public Component()
+        {
+            EngineCalls.print("component in C#");
+            enabled = true;
+        }
 
         public Component(UIntPtr instance)
         {
             CplusplusInstance = instance;
+            enabled = true;
         }
+
+        public abstract void Start();
+        
+        public abstract void Update(float deltaTime);
+
+        public abstract void Destroy();
+
+        [System.Runtime.CompilerServices.MethodImplAttribute(System.Runtime.CompilerServices.MethodImplOptions.InternalCall)]
+        private extern void SetEnabled(UIntPtr instance, bool enabled);
+
+        [System.Runtime.CompilerServices.MethodImplAttribute(System.Runtime.CompilerServices.MethodImplOptions.InternalCall)]
+        private extern void SetName(UIntPtr instance, string name);
     }
 }

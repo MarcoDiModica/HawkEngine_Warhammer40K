ede632ec3c5b8f47596a2b0c902337ed25fdd51e
SalvaBernades <salvador.bernades@estudiantat.upc.edu>
2025-01-23 17:37:35 +0100

    Arreglo del constructor

--- MyGameMaker/MyShadersEngine/ShaderComponent.h
+++ MyGameMaker/MyShadersEngine/ShaderComponent.h
@@ -4,22 +4,22 @@
 #include "MyGameEngine/Material.h"
 #include "MyGameEngine/types.h"
 
-class ShaderComponent : public Component {
+class ShaderComponent {
 public:
     ShaderComponent();
     ~ShaderComponent();
 
-    void SetShader(Shaders * newShader);
-	Shaders* GetShader() const;
+    void SetShader(Shaders* newShader);
+    Shaders* GetShader() const;
 
-	void SetType(ShaderType newType);
-	ShaderType GetType() const;
+    void SetType(ShaderType newType);
+    ShaderType GetType() const;
 
-	void SetOwnerMaterial(Material* newOwnerMaterial);
-	Material* GetOwnerMaterial() const;
+    void SetOwnerMaterial(Material* newOwnerMaterial);
+    Material* GetOwnerMaterial() const;
 
 private:
-    ShaderType type;
     Shaders* shader;
-	Material* ownerMaterial;
+    ShaderType type;
+    Material* ownerMaterial;
 };

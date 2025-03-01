b3e7e4f2a1aff38457e369d3ce63113b5b2a69e2
Marco Di Modica Elias <marcodimodica@protonmail.com>
2025-03-01 11:28:11 +0100

    ups

--- MyGameMaker/Script/PlayerShooting.cs
+++ MyGameMaker/Script/PlayerShooting.cs
@@ -67,7 +67,6 @@ public class PlayerShooting : MonoBehaviour
             GameObject projectile = Engineson.CreateGameObject("Projectile", null);
 
             // TODO: add mesh to the projectile
-            projectile.AddComponent<MeshRenderer>();
 
             if (projectile != null)
             {

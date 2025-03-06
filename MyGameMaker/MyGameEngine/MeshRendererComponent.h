4fd56942af7157390ef398e4e2de55a32a5458ef
Marco Di Modica Elias <marcodimodica@protonmail.com>
2025-03-06 01:06:23 +0100

    begginig of the encoding of meshrenderer, not working yet

--- MyGameMaker/MyGameEngine/MeshRendererComponent.h
+++ MyGameMaker/MyGameEngine/MeshRendererComponent.h
@@ -6,11 +6,14 @@
 #include "Material.h"
 #include "Image.h"
 #include "../MyGameEditor/Log.h"
+#include "../MyShadersEngine/ShaderComponent.h"
+#include "GameObject.h"
 
 class Mesh;
 class Material;
 class Image;
-class Shaders;
+class ShaderComponent;
+class GameObject;
 
 class MeshRenderer : public Component {
 public:
@@ -52,7 +55,8 @@ private:
 protected:
 	friend class SceneSerializer;
 
-	YAML::Node encode() override {
+	YAML::Node encode() override
+	{
 		YAML::Node node = Component::encode();
 
 		if (mesh) {
@@ -73,61 +77,78 @@ protected:
 			colorNode.push_back(material->GetColor().a);
 			node["color"] = colorNode;
 
+			node["shader_type"] = static_cast<int>(material->shaderType);
+
 			if (material->imagePtr && !material->imagePtr->image_path.empty()) {
 				node["image_path"] = material->imagePtr->image_path;
 			}
 		}
 
+		if (owner->HasComponent<ShaderComponent>()) {
+			auto shaderComp = owner->GetComponent<ShaderComponent>();
+
+			node["shader_frequency"] = shaderComp->frequency;
+			node["shader_amplitude"] = shaderComp->amplitude;
+		}
+
 		return node;
 	}
 
-	bool decode(const YAML::Node& node) {
+	bool decode(const YAML::Node& node) override
+	{
 		if (!Component::decode(node)) {
 			return false;
 		}
 
-		if (!node["mesh_path"]) {
-			LOG(LogType::LOG_ERROR, "MeshRenderer: Missing mesh_path in node");
-			return false;
-		}
-
-		std::string path = node["mesh_path"].as<std::string>();
 		std::shared_ptr<Mesh> _mesh = nullptr;
 
-		if (path.substr(0, 6) == "shapes" || path.substr(0, 5) == "shape") {
-			if (path.find("cube") != std::string::npos) {
-				_mesh = Mesh::CreateCube();
-				LOG(LogType::LOG_INFO, "MeshRenderer: Created cube mesh");
-			}
-			else if (path.find("sphere") != std::string::npos) {
-				_mesh = Mesh::CreateSphere();
-				LOG(LogType::LOG_INFO, "MeshRenderer: Created sphere mesh");
-			}
-			else if (path.find("plane") != std::string::npos) {
-				_mesh = Mesh::CreatePlane();
-				LOG(LogType::LOG_INFO, "MeshRenderer: Created plane mesh");
-			}
-			else if (path.find("cylinder") != std::string::npos) {
-				_mesh = Mesh::CreateCylinder();
-				LOG(LogType::LOG_INFO, "MeshRenderer: Created cylinder mesh");
-			}
-		}
-		else if (node["mesh_name"]) {
-			std::string meshName = node["mesh_name"].as<std::string>();
-			_mesh = Mesh::LoadBinary(meshName);
+		if (node["mesh_path"]) {
+			std::string path = node["mesh_path"].as<std::string>();
 
-			if (!_mesh) {
-				LOG(LogType::LOG_ERROR, "MeshRenderer: Failed to load mesh %s", meshName.c_str());
-				_mesh = Mesh::CreateCube();
-			}
-		}
+			std::string lowerPath = path;
+			for (auto& c : lowerPath) c = std::tolower(c);
 
-		if (_mesh) {
-			SetMesh(_mesh);
-		}
-		else {
-			LOG(LogType::LOG_ERROR, "MeshRenderer: Failed to create or load mesh");
-			return false;
+			if (lowerPath.find("shape") != std::string::npos || lowerPath.find("shapes") != std::string::npos) {
+				if (lowerPath.find("cube") != std::string::npos) {
+					_mesh = Mesh::CreateCube();
+					LOG(LogType::LOG_INFO, "MeshRenderer: Created cube mesh");
+				}
+				else if (lowerPath.find("sphere") != std::string::npos) {
+					_mesh = Mesh::CreateSphere();
+					LOG(LogType::LOG_INFO, "MeshRenderer: Created sphere mesh");
+				}
+				else if (lowerPath.find("plane") != std::string::npos) {
+					_mesh = Mesh::CreatePlane();
+					LOG(LogType::LOG_INFO, "MeshRenderer: Created plane mesh");
+				}
+				else if (lowerPath.find("cylinder") != std::string::npos) {
+					_mesh = Mesh::CreateCylinder();
+					LOG(LogType::LOG_INFO, "MeshRenderer: Created cylinder mesh");
+				}
+				else {
+					LOG(LogType::LOG_WARNING, "MeshRenderer: Unknown primitive shape, defaulting to cube");
+					_mesh = Mesh::CreateCube();
+				}
+			}
+			else if (node["mesh_name"]) {
+				std::string meshName = node["mesh_name"].as<std::string>();
+				try {
+					_mesh = Mesh::LoadBinary(meshName);
+					if (!_mesh) {
+						LOG(LogType::LOG_ERROR, "MeshRenderer: Failed to load mesh %s", meshName.c_str());
+						_mesh = Mesh::CreateCube();
+					}
+				}
+				catch (const std::exception& e) {
+					LOG(LogType::LOG_ERROR, "MeshRenderer: Error loading mesh %s: %s",
+						meshName.c_str(), e.what());
+					_mesh = Mesh::CreateCube();
+				}
+			}
+
+			if (_mesh) {
+				SetMesh(_mesh);
+			}
 		}
 
 		std::shared_ptr<Material> _material = std::make_shared<Material>();
@@ -141,7 +162,6 @@ protected:
 			catch (const std::exception& e) {
 				LOG(LogType::LOG_ERROR, "MeshRenderer: Error loading material %s: %s",
 					materialPath.c_str(), e.what());
-				_material = std::make_shared<Material>();
 			}
 		}
 
@@ -154,13 +174,20 @@ protected:
 			_material->SetColor(color);
 		}
 
+		if (node["shader_type"]) {
+			ShaderType shaderType = static_cast<ShaderType>(node["shader_type"].as<int>());
+			_material->shaderType = shaderType;
+		}
+
+		SetMaterial(_material);
+
 		if (node["image_path"]) {
 			std::string imagePath = node["image_path"].as<std::string>();
 			auto image = std::make_shared<Image>();
 			image->LoadTexture(imagePath);
 
 			if (image->id() != 0) {
-				_material->setImage(image);
+				SetImage(image);
 				LOG(LogType::LOG_INFO, "MeshRenderer: Loaded image %s", imagePath.c_str());
 			}
 			else {
@@ -168,8 +195,30 @@ protected:
 			}
 		}
 
-		SetMaterial(_material);
+		auto shaderComponent = owner->GetComponent<ShaderComponent>();
+		if (shaderComponent) {
+			shaderComponent->SetOwnerMaterial(material.get());
+
+			if (node["shader_type"]) {
+				ShaderType shaderType = static_cast<ShaderType>(node["shader_type"].as<int>());
+				shaderComponent->SetShaderType(shaderType);
+			}
+			else {
+				shaderComponent->SetShaderType(ShaderType::LIGHT);
+			}
+
+			if (node["shader_frequency"]) {
+				shaderComponent->frequency = node["shader_frequency"].as<float>();
+			}
+
+			if (node["shader_amplitude"]) {
+				shaderComponent->amplitude = node["shader_amplitude"].as<float>();
+			}
+		}
+		else {
+			LOG(LogType::LOG_ERROR, "MeshRenderer: ShaderComponent not found, but should have been created in constructor");
+		}
+
 		return true;
 	}
-
 };

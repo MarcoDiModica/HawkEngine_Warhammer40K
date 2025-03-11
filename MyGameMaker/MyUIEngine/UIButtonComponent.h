8b3b65749964e1872e5546c62616ea51d77041fc
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2025-03-11 09:49:02 +0100

    components now have the encode and decode functions

--- MyGameMaker/MyUIEngine/UIButtonComponent.h
+++ MyGameMaker/MyUIEngine/UIButtonComponent.h
@@ -35,5 +35,35 @@ public:
 private:
 	ButtonState state = ButtonState::DEFAULT;
 
+protected:
+
+	friend class SceneSerializer;
+
+	YAML::Node encode() override {
+
+		YAML::Node node = Component::encode();
+
+		node["light_type"] = static_cast<int>(type);
+		//node["color"] = { color.r, color.g, color.b };
+		node["intensity"] = intensity;
+		node["radius"] = radius;
+		//node["direction"] = { direction.x, direction.y, direction.z };
+
+		return node;
+	}
+
+	bool decode(const YAML::Node& node) override {
+
+		Component::decode(node);
+
+		type = static_cast<LightType>(node["light_type"].as<int>());
+		//color = { node["color"][0].as<float>(), node["color"][1].as<float>(), node["color"][2].as<float>() };
+		intensity = node["intensity"].as<float>();
+		radius = node["radius"].as<float>();
+		//direction = { node["direction"][0].as<float>(), node["direction"][1].as<float>(), node["direction"][2].as<float>() };
+
+		return true;
+	}
+
 };
 

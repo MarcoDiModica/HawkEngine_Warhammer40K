47151391b96f7680e0c35de3e228b53d9a06f318
Marc Avante <avantecaballe@gmail.com>
2025-03-13 01:44:32 +0100

    It compiles now, but crashes on creating scene

--- MyGameMaker/MyGameEngine/Material.h
+++ MyGameMaker/MyGameEngine/Material.h
@@ -7,25 +7,6 @@
 #include "types.h"
 #include "yaml-cpp/yaml.h"
 
-namespace YAML {
-    template <>
-    struct convert<glm::vec2> {
-        static Node encode(const glm::vec2& rhs) {
-            Node node;
-            node.push_back(rhs.x);
-            node.push_back(rhs.y);
-            return node;
-        }
-        static bool decode(const Node& node, glm::vec2& rhs) {
-            if (!node.IsSequence() || node.size() != 2)
-                return false;
-            rhs.x = node[0].as<float>();
-            rhs.y = node[1].as<float>();
-            return true;
-        }
-    };
-}
-
 class Material
 {
 public:

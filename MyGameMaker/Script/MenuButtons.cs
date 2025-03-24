d3c6065302f8a3f496e31c840bec883c7822f187
D1dii <didacgp123@gmail.com>
2025-03-24 13:31:51 +0100

    Some comments needed to build

--- MyGameMaker/Script/MenuButtons.cs
+++ MyGameMaker/Script/MenuButtons.cs
@@ -33,37 +33,35 @@ public class MenuButtons : MonoBehaviour
         if (newGameButton.GetState() == ButtonState.CLICKED)
         {
            SceneManager.LoadScene("Level2");
-            sound?.LoadAudio(buttonClicked);
-            sound?.Play();
         }
         
-        if (continueButton.GetState() == ButtonState.CLICKED)
-        {
-            sound?.LoadAudio(buttonClicked);
-            sound?.Play();
-            Engineson.print("Continue is Clicked");
-        }
+        //if (continueButton.GetState() == ButtonState.CLICKED)
+        //{
+        //    sound?.LoadAudio(buttonClicked);
+        //    sound?.Play();
+        //    Engineson.print("Continue is Clicked");
+        //}
 
-        if (optionsButton.GetState() == ButtonState.CLICKED)
-        {
-            sound?.LoadAudio(buttonClicked);
-            sound?.Play();
-            Engineson.print("Options is Clicked");
-        }
+        //if (optionsButton.GetState() == ButtonState.CLICKED)
+        //{
+        //    sound?.LoadAudio(buttonClicked);
+        //    sound?.Play();
+        //    Engineson.print("Options is Clicked");
+        //}
 
-        if (creditsButton.GetState() == ButtonState.CLICKED)
-        {
-            sound?.LoadAudio(buttonClicked);
-            sound?.Play();
-            Engineson.print("Credits is Clicked");
-        }
+        //if (creditsButton.GetState() == ButtonState.CLICKED)
+        //{
+        //    sound?.LoadAudio(buttonClicked);
+        //    sound?.Play();
+        //    Engineson.print("Credits is Clicked");
+        //}
 
-        if (quitButton.GetState() == ButtonState.CLICKED)
-        {
-            sound?.LoadAudio(buttonClicked);
-            sound?.Play();
-            Engineson.print("quit is Clicked");
-        }
+        //if (quitButton.GetState() == ButtonState.CLICKED)
+        //{
+        //    sound?.LoadAudio(buttonClicked);
+        //    sound?.Play();
+        //    Engineson.print("quit is Clicked");
+        //}
 
     }
 }

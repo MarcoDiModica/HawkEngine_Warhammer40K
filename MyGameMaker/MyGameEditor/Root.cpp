e3e6ce5f5dd1711cb040ddf1cdf8c8f4449326dd
Ivomm9 <ivomontesmoreda@gmail.com>
2025-04-03 15:14:51 +0200

    weapons and bible pages pickups done

--- MyGameMaker/MyGameEditor/Root.cpp
+++ MyGameMaker/MyGameEditor/Root.cpp
@@ -62,42 +62,42 @@ bool Root::CleanUp()
 
 bool Root::Start()
 {
-	auto player = CreateGameObject("Player");
-	player->GetTransform()->SetPosition(glm::vec3(0, 0, 0));
-	player->AddComponent<ScriptComponent>()->LoadScript("PlayerShooting");
-	player->AddComponent<ScriptComponent>()->LoadScript("PlayerMovement");
-	player->AddComponent<ScriptComponent>()->LoadScript("PlayerInput");
-	player->AddComponent<ScriptComponent>()->LoadScript("PlayerDash");
-	player->AddComponent<ScriptComponent>()->LoadScript("PlayerController");
-	player->AddComponent<ScriptComponent>()->LoadScript("PlayerPowerUp");
-	player->AddComponent<ScriptComponent>()->LoadScript("Boltgun");
-	player->AddComponent<ScriptComponent>()->LoadScript("Shotgun");
-	player->AddComponent<ScriptComponent>()->LoadScript("GrenadeLauncher");
-	player->AddComponent<ScriptComponent>()->LoadScript("Railgun");
-	player->AddComponent<ScriptComponent>()->LoadScript("LaserBeam");
-	player->AddComponent<ScriptComponent>()->LoadScript("EnergyBall");
-	player->AddComponent<ScriptComponent>()->LoadScript("ToggleMode");
-	player->AddComponent<ScriptComponent>()->LoadScript("Barrage");
-	player->AddComponent<ScriptComponent>()->LoadScript("HookShot");
-	player->AddComponent<ScriptComponent>()->LoadScript("ArcSnare");
-	player->AddComponent<ScriptComponent>()->LoadScript("RedThirstManager");
-
-	player->AddComponent<SoundComponent>()->LoadAudio("Assets/Audio/SFX/Weapons/Boltgun/BoltgunShot.wav", true);
-
-	auto playerMesh = CreateGameObjectWithPath("Assets/Meshes/MainCharacterAnimated.fbx");
-	playerMesh->SetName("playerMesh");
-	playerMesh->GetTransform()->Rotate(glm::radians(-90.0f), glm::dvec3(1, 0, 0));
-	playerMesh->GetTransform()->SetScale(glm::vec3(1, 1, 1));
-	playerMesh->GetTransform()->SetPosition(glm::vec3(0, 0, 0));
-	ParentGameObject(*playerMesh, *player);
-	playerMesh->AddComponent<ScriptComponent>()->LoadScript("PlayerAnimations");
-	player->AddComponent<CapsuleColliderComponent>(Application->physicsModule);
-	player->AddComponent<RigidbodyComponent>(Application->physicsModule);
-	player->GetComponent<RigidbodyComponent>()->SetFreezeRotations(true);
-	player->GetComponent<RigidbodyComponent>()->SetGravity(glm::vec3(0, -200, 0));
-	player->GetComponent<CapsuleColliderComponent>()->SetSize(glm::vec3(1.7f, 1.1f, 1));
-	player->GetComponent<CapsuleColliderComponent>()->SetOffset(glm::vec3(0, 2.1f, 0));
-	player->AddComponent<ScriptComponent>()->LoadScript("InteractionSystem");
+	//auto player = CreateGameObject("Player");
+	//player->GetTransform()->SetPosition(glm::vec3(0, 0, 0));
+	//player->AddComponent<ScriptComponent>()->LoadScript("PlayerShooting");
+	//player->AddComponent<ScriptComponent>()->LoadScript("PlayerMovement");
+	//player->AddComponent<ScriptComponent>()->LoadScript("PlayerInput");
+	//player->AddComponent<ScriptComponent>()->LoadScript("PlayerDash");
+	//player->AddComponent<ScriptComponent>()->LoadScript("PlayerController");
+	//player->AddComponent<ScriptComponent>()->LoadScript("PlayerPowerUp");
+	//player->AddComponent<ScriptComponent>()->LoadScript("Boltgun");
+	//player->AddComponent<ScriptComponent>()->LoadScript("Shotgun");
+	//player->AddComponent<ScriptComponent>()->LoadScript("GrenadeLauncher");
+	//player->AddComponent<ScriptComponent>()->LoadScript("Railgun");
+	//player->AddComponent<ScriptComponent>()->LoadScript("LaserBeam");
+	//player->AddComponent<ScriptComponent>()->LoadScript("EnergyBall");
+	//player->AddComponent<ScriptComponent>()->LoadScript("ToggleMode");
+	//player->AddComponent<ScriptComponent>()->LoadScript("Barrage");
+	//player->AddComponent<ScriptComponent>()->LoadScript("HookShot");
+	//player->AddComponent<ScriptComponent>()->LoadScript("ArcSnare");
+	//player->AddComponent<ScriptComponent>()->LoadScript("RedThirstManager");
+	//
+	//player->AddComponent<SoundComponent>()->LoadAudio("Assets/Audio/SFX/Weapons/Boltgun/BoltgunShot.wav", true);
+	//
+	//auto playerMesh = CreateGameObjectWithPath("Assets/Meshes/MainCharacterAnimated.fbx");
+	//playerMesh->SetName("playerMesh");
+	//playerMesh->GetTransform()->Rotate(glm::radians(-90.0f), glm::dvec3(1, 0, 0));
+	//playerMesh->GetTransform()->SetScale(glm::vec3(1, 1, 1));
+	//playerMesh->GetTransform()->SetPosition(glm::vec3(0, 0, 0));
+	//ParentGameObject(*playerMesh, *player);
+	//playerMesh->AddComponent<ScriptComponent>()->LoadScript("PlayerAnimations");
+	//player->AddComponent<CapsuleColliderComponent>(Application->physicsModule);
+	//player->AddComponent<RigidbodyComponent>(Application->physicsModule);
+	//player->GetComponent<RigidbodyComponent>()->SetFreezeRotations(true);
+	//player->GetComponent<RigidbodyComponent>()->SetGravity(glm::vec3(0, -200, 0));
+	//player->GetComponent<CapsuleColliderComponent>()->SetSize(glm::vec3(1.7f, 1.1f, 1));
+	//player->GetComponent<CapsuleColliderComponent>()->SetOffset(glm::vec3(0, 2.1f, 0));
+	//player->AddComponent<ScriptComponent>()->LoadScript("InteractionSystem");
 	
 
 
@@ -243,29 +243,29 @@ bool Root::Start()
 	//CreateGameplayUI();
 	//CreateMainMenuUI();
 
-	auto shotgunPickUp = CreateGameObjectWithPath("Assets/shotgun.fbx");
-	shotgunPickUp->GetTransform()->SetPosition(glm::vec3(30, 2, 0));
-	shotgunPickUp->GetTransform()->SetScale(glm::vec3(0.003, 0.003, 0.003));
-	shotgunPickUp->AddComponent<BoxColliderComponent>(Application->physicsModule);
-	shotgunPickUp->GetComponent<BoxColliderComponent>()->SetTrigger(true);
-	shotgunPickUp->AddComponent<ScriptComponent>()->LoadScript("ShotgunPickUp");
-	shotgunPickUp->SetTag("Weapon");
-
-	auto railgunPickUp = CreateGameObjectWithPath("Assets/railgun.fbx");
-	railgunPickUp->GetTransform()->SetPosition(glm::vec3(30, 2, 0));
-	railgunPickUp->GetTransform()->SetScale(glm::vec3(0.003, 0.003, 0.003));
-	railgunPickUp->AddComponent<BoxColliderComponent>(Application->physicsModule);
-	railgunPickUp->GetComponent<BoxColliderComponent>()->SetTrigger(true);
-	railgunPickUp->AddComponent<ScriptComponent>()->LoadScript("RailgunPickUp");
-	railgunPickUp->SetTag("Weapon");
-
-	auto boltgunPickUp = CreateGameObjectWithPath("Assets/boltgun.fbx");
-	boltgunPickUp->GetTransform()->SetPosition(glm::vec3(30, 2, 0));
-	boltgunPickUp->GetTransform()->SetScale(glm::vec3(0.003, 0.003, 0.003));
-	boltgunPickUp->AddComponent<BoxColliderComponent>(Application->physicsModule);
-	boltgunPickUp->GetComponent<BoxColliderComponent>()->SetTrigger(true);
-	boltgunPickUp->AddComponent<ScriptComponent>()->LoadScript("BoltgunPickUp");
-	boltgunPickUp->SetTag("Weapon");
+	//auto shotgunPickUp = CreateGameObjectWithPath("Assets/shotgun.fbx");
+	//shotgunPickUp->GetTransform()->SetPosition(glm::vec3(30, 2, 0));
+	//shotgunPickUp->GetTransform()->SetScale(glm::vec3(0.003, 0.003, 0.003));
+	//shotgunPickUp->AddComponent<BoxColliderComponent>(Application->physicsModule);
+	//shotgunPickUp->GetComponent<BoxColliderComponent>()->SetTrigger(true);
+	//shotgunPickUp->AddComponent<ScriptComponent>()->LoadScript("ShotgunPickUp");
+	//shotgunPickUp->SetTag("Weapon");
+	//
+	//auto railgunPickUp = CreateGameObjectWithPath("Assets/railgun.fbx");
+	//railgunPickUp->GetTransform()->SetPosition(glm::vec3(30, 2, 0));
+	//railgunPickUp->GetTransform()->SetScale(glm::vec3(0.003, 0.003, 0.003));
+	//railgunPickUp->AddComponent<BoxColliderComponent>(Application->physicsModule);
+	//railgunPickUp->GetComponent<BoxColliderComponent>()->SetTrigger(true);
+	//railgunPickUp->AddComponent<ScriptComponent>()->LoadScript("RailgunPickUp");
+	//railgunPickUp->SetTag("Weapon");
+	//
+	//auto boltgunPickUp = CreateGameObjectWithPath("Assets/boltgun.fbx");
+	//boltgunPickUp->GetTransform()->SetPosition(glm::vec3(30, 2, 0));
+	//boltgunPickUp->GetTransform()->SetScale(glm::vec3(0.003, 0.003, 0.003));
+	//boltgunPickUp->AddComponent<BoxColliderComponent>(Application->physicsModule);
+	//boltgunPickUp->GetComponent<BoxColliderComponent>()->SetTrigger(true);
+	//boltgunPickUp->AddComponent<ScriptComponent>()->LoadScript("BoltgunPickUp");
+	//boltgunPickUp->SetTag("Weapon");
 
 #ifdef _BUILD
 	Application->play = true;

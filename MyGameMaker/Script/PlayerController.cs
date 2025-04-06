2a3d8c3bcf18c228a0ae2bf20cdb38f5da2a0fa1
joanmarquesbesses <85459149+joanmarquesbesses@users.noreply.github.com>
2025-04-06 19:05:47 +0200

    when player dies go to losescreen

--- MyGameMaker/Script/PlayerController.cs
+++ MyGameMaker/Script/PlayerController.cs
@@ -91,6 +91,7 @@ public class PlayerController : MonoBehaviour
                     playerAnimations.SetDeathAnimation();
                     sound.LoadAudio(DeathAudio);
                     sound.Play();
+                    SceneManager.LoadScene("LoseScene");
                 }
                 else
                 {

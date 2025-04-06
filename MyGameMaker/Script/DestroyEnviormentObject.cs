2ed88368f409c38430f88965cdbec8c3d131b0e2
Biel <biel.linan42@gmail.com>
2025-04-07 01:03:22 +0200

    destroy enviorment script

---
+++ MyGameMaker/Script/DestroyEnviormentObject.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using HawkEngine;
+
+public class DestroyEnviormentObject : MonoBehaviour
+{
+
+    public void DestroyObject()
+    {
+        gameObject.GetComponent<Transform>().position = new Vector3(0, -100, 0);
+        gameObject.GetComponent<Collider>().SetTrigger(true);
+    }
+
+    public override void Awake()
+    {
+        base.Awake();
+    }
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update(float deltaTime)
+    {
+        base.Update(deltaTime);
+    }
+
+}

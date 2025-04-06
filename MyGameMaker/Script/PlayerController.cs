af191bc8e9c34b1087d81a53e1c07d249f2acd19
Yeeway69 <yeyiwei1@gmail.com>
2025-04-07 01:30:38 +0200

    mor audio fix

--- MyGameMaker/Script/PlayerController.cs
+++ MyGameMaker/Script/PlayerController.cs
@@ -37,7 +37,7 @@ public class PlayerController : MonoBehaviour
     private string Runfootsteps = "Assets/Audio/SFX/Player/PlayerFootstep_ready.wav";
     private string Walkfootsteps = "Assets/Audio/SFX/Player/PlayerWalkFootstep_ready.wav";
     public string HitAudio = "Assets/Audio/SFX/Player/PlayerHit_ready.wav";
-    public string DeathAudio = "Assets/Audio/SFX/Player/PlayerDash_ready.wav";
+    public string DeathAudio = "Assets/Audio/SFX/PlayerPlayerDeath_ready.wav";
 
     private ParticleFX inactiveDashFX;
     private ParticleFX walkingFX;
--- MyGameMaker/Script/PlayerDash.cs
+++ MyGameMaker/Script/PlayerDash.cs
@@ -24,7 +24,7 @@ public class PlayerDash : MonoBehaviour
     private float targetFOV;
     private float zoomSpeed = 0.5f;
     private Audio sound;
-    private string DashSound = "Assets/Audio/SFX/Player/PlayerDash.wav";
+    private string DashSound = "Assets/Audio/SFX/Player/PlayerDash_ready.wav";
 
     
 

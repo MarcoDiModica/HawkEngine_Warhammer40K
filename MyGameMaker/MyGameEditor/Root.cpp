80fcc5a84ae51030f336c5fa4fcd58f4efe7f1f3
Peluchomp <pau.mora.sole@gmail.com>
2025-04-07 18:50:58 +0200

    Some more bugfixes

--- MyGameMaker/MyGameEditor/Root.cpp
+++ MyGameMaker/MyGameEditor/Root.cpp
@@ -108,6 +108,8 @@ bool Root::Start()
 	//player->AddComponent<ScriptComponent>()->LoadScript("ArcSnare");
 	//player->AddComponent<ScriptComponent>()->LoadScript("RedThirstManager");
 	
+
+	
 	
 	//auto canvasMainMenu = FindGOByName("Canvas_Main_Menu");
 	//canvasMainMenu->AddComponent<ScriptComponent>()->LoadScript("MenuButtons");

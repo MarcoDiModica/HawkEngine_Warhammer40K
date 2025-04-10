102f09cbb2789262b652d26d88817ada765a20eb
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2025-04-10 12:01:32 +0200

    final iteration menu inputs with controller

--- MyGameMaker/Script/MenuButtons.cs
+++ MyGameMaker/Script/MenuButtons.cs
@@ -1,5 +1,6 @@
 using HawkEngine;
 using System.Numerics;
+using System.Runtime.InteropServices;
 
 public class MenuButtons : MonoBehaviour
 {
@@ -29,9 +30,9 @@ public class MenuButtons : MonoBehaviour
     private ButtonState prevState_optionsButton = ButtonState.DEFAULT;
     private ButtonState prevState_quitButton = ButtonState.DEFAULT;
 
+    private bool[] hasPlayedHoverSound;
 
     private Audio sound;
-    //private Audio sound;
     private string buttonHovered = "Assets/Audio/SFX/UI/UI_Hover.wav";
     private string buttonClicked = "Assets/Audio/SFX/UI/UI_Click.wav";
     private string buttonStartGame = "Assets/Audio/SFX/UI/UI_Confirm.wav";
@@ -39,10 +40,12 @@ public class MenuButtons : MonoBehaviour
     private int selectedButtonIndex = 0;
     private UIButton[] buttons;
     private UITransform[] transforms;
+
     public override void Awake()
     {
 
     }
+
     public override void Start()
     {
         newGameButton = GameObject.Find("new_game_button");
@@ -64,6 +67,8 @@ public class MenuButtons : MonoBehaviour
 
         buttons = new UIButton[] { button_newGameButton, button_continueButton, button_optionsButton, button_quitButton };
         transforms = new UITransform[] { transform_newGameButton, transform_continueButton, transform_optionsButton, transform_quitButton };
+
+        hasPlayedHoverSound = new bool[buttons.Length];
 
         if (newGameButton == null || optionsButton == null || creditsButton == null || quitButton == null)
         {
@@ -105,21 +110,52 @@ public class MenuButtons : MonoBehaviour
             {
                 buttons[i].SetState(ButtonState.HOVERED);
                 transforms[i].DOScaleUI(new Vector3(0.25f, 0.1f, 0.5f), 0.3f, Modes.EASE_OUT);
+
+                if (!hasPlayedHoverSound[i])
+                {
+                    sound?.LoadAudio(buttonHovered);
+                    sound?.Play();
+                    hasPlayedHoverSound[i] = true;
+                }
             }
             else
             {
                 buttons[i].SetState(ButtonState.DEFAULT);
                 transforms[i].DOScaleUI(new Vector3(0.182f, 0.070f, 0.4f), 0.3f, Modes.EASE_OUT);
+                hasPlayedHoverSound[i] = false;
             }
-
         }
 
         if (Input.GetControllerButtonDown(ControllerButton.A))
         {
-            buttons[selectedButtonIndex].SetState(ButtonState.CLICKED);
-        }
-        Engineson.print("Selected Button Index: " + selectedButtonIndex);
+            UIButton selectedButton = buttons[selectedButtonIndex];
+            selectedButton.SetState(ButtonState.CLICKED);
 
+            if (selectedButton == button_newGameButton)
+            {
+                sound?.LoadAudio(buttonStartGame);
+                sound?.Play();
+                SceneManager.LoadScene("Mortis_Level1");
+            }
+            else if (selectedButton == button_continueButton)
+            {
+                sound?.LoadAudio(buttonStartGame);
+                sound?.Play();
+                SceneManager.LoadScene("SpaceShip");
+            }
+            else if (selectedButton == button_optionsButton)
+            {
+                sound?.LoadAudio(buttonClicked);
+                sound?.Play();
+                optionsCanvas.SetActive(true);
+            }
+            else if (selectedButton == button_quitButton)
+            {
+                sound?.LoadAudio(buttonClicked);
+                sound?.Play();
+                // Aquí puedes agregar la lógica para salir del juego
+            }
+        }
     }
 
     public override void Update(float deltaTime)
@@ -141,37 +177,6 @@ public class MenuButtons : MonoBehaviour
         }
 
         NavigateMenu();
-
-        if (button_newGameButton.GetState() == ButtonState.CLICKED)
-        {
-            sound?.LoadAudio(buttonStartGame);
-            sound?.Play();
-            SceneManager.LoadScene("Mortis_Level1");
-        }
-
-        
-        if (button_continueButton.GetState() == ButtonState.CLICKED)
-        {
-            sound?.LoadAudio(buttonStartGame);
-            sound?.Play();
-            SceneManager.LoadScene("SpaceShip");
-        }
-
-        
-        if (button_optionsButton.GetState() == ButtonState.CLICKED)
-        {
-            sound?.LoadAudio(buttonClicked);
-            sound?.Play();
-            optionsCanvas.SetActive(true);
-        }
-
-        
-        if (button_quitButton.GetState() == ButtonState.CLICKED)
-        {
-            sound?.LoadAudio(buttonClicked);
-            sound?.Play();
-        }
-
-        
     }
 }
+

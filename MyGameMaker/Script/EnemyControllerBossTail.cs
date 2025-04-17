88d0d035ce83058970dae787c46951dcc6612c00
Jordopol <jordi.tur@estudiantat.upc.edu>
2025-04-17 21:56:34 +0200

    Declared BossTail enemy class

---
+++ MyGameMaker/Script/EnemyControllerBossTail.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using HawkEngine;
+
+public class EnemyControllerBossTail : EnemyController
+{
+
+    public override void Awake()
+    {
+
+    }
+
+    public override void Start()
+    {
+
+    }
+
+    public override void Update(float deltaTime)
+    {
+
+    }
+
+    public override void Attack()
+    {
+
+    }
+
+    public override void TakeDamage(float damage)
+    {
+
+    }
+
+}

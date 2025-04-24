f28acd76c4d6d76c2eb2fd7896d84746659d7c01
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2025-04-24 20:57:01 +0200

    Update TextComponent.cpp

--- MyGameMaker/MyUIEngine/TextComponent.cpp
+++ MyGameMaker/MyUIEngine/TextComponent.cpp
@@ -5,6 +5,7 @@
 #include "../MyGameEditor/UIGameView.h"
 #include "../MyUIEngine/FontManager.h"
 #include "../MyUIEngine/UITransformComponent.h"
+#include "../MyGameEngine/CameraComponent.h"
 
 TextComponent::TextComponent(GameObject* owner, const std::string& text, const glm::vec2& position, const glm::vec3& color, float fontSize)
     : Component(owner), m_owner(owner), m_text(text), m_position(position), m_color(color), m_fontSize(fontSize)
@@ -58,49 +59,39 @@ float TextComponent::GetFontSize() const
 
 void TextComponent::Render() const
 {
+    // Obtener el shader
     Shaders* unlitShader = ShaderManager::GetInstance().GetShader(ShaderType::UNLIT);
-
-    if (!unlitShader)
-    {
+    if (!unlitShader) {
         std::cerr << "ERROR: Shader UNLIT no encontrado." << std::endl;
         return;
     }
 
-    unlitShader->Bind();
-    unlitShader->SetUniform("uTextColor", m_color);
-	unlitShader->SetUniform("u_HasTexture", false);
+    CameraComponent* mainCamera = Application->root->mainCamera->GetComponent<CameraComponent>();
+    if (!mainCamera) {
+        std::cerr << "ERROR: No se encontró la cámara principal." << std::endl;
+        return;
+    }
 
-    glm::vec2 renderPosition = m_position; 
+    glm::mat4 projectionMatrix = mainCamera->projection();
+    glm::mat4 viewMatrix = mainCamera->view();
+
+    unlitShader->Bind();
+    unlitShader->SetUniform("uProjection", projectionMatrix);
+    unlitShader->SetUniform("uView", viewMatrix);
+
+    glm::vec2 renderPosition = m_position;
     if (m_owner->HasComponent<UITransformComponent>()) {
         UITransformComponent* rectTransform = m_owner->GetComponent<UITransformComponent>();
         renderPosition = glm::vec2(rectTransform->GetPosition().x, rectTransform->GetPosition().y);
     }
-	FontManager::GetInstance().LoadFont("assets/arial_narrow_7.ttf", m_fontSize);
+
+    FontManager::GetInstance().LoadFont("assets/arial_narrow_7.ttf", m_fontSize);
     FontManager::GetInstance().RenderText(unlitShader, m_text, renderPosition.x, renderPosition.y, m_fontSize, m_color);
 
     unlitShader->UnBind();
 
     std::cout << "Renderizando texto: " << m_text
-        << " en posición (" << m_position.x << ", " << m_position.y << ")"
+        << " en posición (" << renderPosition.x << ", " << renderPosition.y << ")"
         << " con color (" << m_color.r << ", " << m_color.g << ", " << m_color.b << ")"
         << " y tamaño de fuente " << m_fontSize << std::endl;
-}
-
-void TextComponent::Update(float deltaTime)
-{
-	if (CsharpReference) {
-		MonoClass* textClass = mono_object_get_class(CsharpReference);
-		MonoMethod* updateMethod = mono_class_get_method_from_name(textClass, "Update", 0);
-		if (updateMethod) {
-			MonoObject* exception = nullptr;
-			mono_runtime_invoke(updateMethod, CsharpReference, nullptr, &exception);
-			if (exception) {
-				MonoString* exceptionMessage = mono_object_to_string(exception, nullptr);
-				const char* exceptionStr = mono_string_to_utf8(exceptionMessage);
-				LOG(LogType::LOG_ERROR, "UpdateError: %s", exceptionStr);
-				mono_free((void*)exceptionStr);
-			}
-		}
-	}
-	Render();
 }

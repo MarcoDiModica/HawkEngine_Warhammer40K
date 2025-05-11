7fbd6ca4818cf7f044c3b87c1ca09284d0214f85
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2025-05-11 22:08:32 +0200

    Lvl1 music

---
+++ MyGameMaker/Script/AudioLevel1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using HawkEngine;
+
+public class AudioLevel1 : MonoBehaviour
+{
+        private string Lvl1Music = "Assets/Audio/Music/Level1_AmbientMusic_No_1_BetaBuild2.ogg";
+        private bool hasStartedMusic = false;
+
+    public override void Update(float deltaTime)
+    {
+        if (!hasStartedMusic)
+        {
+            Audio.Play(Lvl1Music, true);
+            hasStartedMusic = true;
+        }
+    }
+
+
+
+
+}
+
+

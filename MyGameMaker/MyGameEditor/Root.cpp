11ee44d82d2f92a1f088429c8ebcaca3c39d7400
PabloL2003 <125266074+PabloL2003@users.noreply.github.com>
2025-05-11 15:20:46 +0200

    zona 3 lvl2 (new)

--- MyGameMaker/MyGameEditor/Root.cpp
+++ MyGameMaker/MyGameEditor/Root.cpp
@@ -65,8 +65,8 @@ bool Root::CleanUp()
 
 bool Root::Start()
 {
-	resourceManager->LoadResources(); // Esto no se mutea
-	resourceManager->CreateCube(); // Esto no se mutea
+	//resourceManager->LoadResources(); // Esto no se mutea
+	//resourceManager->CreateCube(); // Esto no se mutea
 
 	//Application->scene_serializer->DeSerialize("Library/Scenes/BetaRelease_Week1_Lvl1.scene");
 
@@ -338,15 +338,15 @@ bool Root::Start()
 	//ParentGameObject(*mawlocTailMesh, *mawlocTail);
 	//mawlocTail->AddComponent<ScriptComponent>()->LoadScript("EnemyControllerBossTail");
 	
-	//auto objMainCamera = CreateCameraObject("MainCamera");
-	//objMainCamera->GetTransform()->SetPosition(glm::dvec3(-14, 20, 14.0f));
-	//objMainCamera->GetTransform()->SetRotation(glm::dvec3(glm::radians(-130.0), glm::radians(45.0), glm::radians(180.0)));
-	//auto camera = objMainCamera->AddComponent<CameraComponent>();
-	//camera->priority = 1;
-	//objMainCamera->AddComponent<ScriptComponent>()->LoadScript("PlayerCamera");
-	//mainCamera = objMainCamera;
-	//UpdateCameraPriority();
-	//
+	auto objMainCamera = CreateCameraObject("MainCamera");
+	objMainCamera->GetTransform()->SetPosition(glm::dvec3(-14, 20, 14.0f));
+	objMainCamera->GetTransform()->SetRotation(glm::dvec3(glm::radians(-130.0), glm::radians(45.0), glm::radians(180.0)));
+	auto camera = objMainCamera->AddComponent<CameraComponent>();
+	camera->priority = 1;
+	objMainCamera->AddComponent<ScriptComponent>()->LoadScript("PlayerCamera");
+	mainCamera = objMainCamera;
+	UpdateCameraPriority();
+	
 	////PLAYER BUENO CON TODOS LOS SCRIPTS PORFA NO LO BORREIS
 	//
 	//auto player = CreateGameObject("Player");

58e9c1f34abf1b2d917b4c62245bd2d51f36d03a
VM Marco <59030832+Marco-v-BaldanII@users.noreply.github.com>
2025-05-31 10:04:24 +0200

    fixed ui and ground

--- MyGameMaker/MyGameEditor/Root.cpp
+++ MyGameMaker/MyGameEditor/Root.cpp
@@ -67,7 +67,7 @@ bool Root::Start()
 	//resourceManager->LoadTextures();
 	//resourceManager->UpdateTextures();
 	//Application->scene_serializer->DeSerialize("Library/Scenes/MainMenu.scene");
-	//Application->scene_serializer->DeSerialize("Library/Scenes/BetaRelease_Week1_Lvl1.scene");
+	Application->scene_serializer->DeSerialize("Library/Scenes/BetaRelease_Week1_Lvl1.scene");
 	//Application->scene_serializer->DeSerialize("Library/Scenes/BetaRelease_Week1_Lvl2.scene");
 	//Application->scene_serializer->DeSerialize("Library/Scenes/BetaRelease_Week1_Bossfight.scene");
 

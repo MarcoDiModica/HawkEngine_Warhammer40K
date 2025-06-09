49300f1442f4ced6a0c8104f7fa1e4591e6394e1
Peluchomp <pau.mora.sole@gmail.com>
2025-06-09 20:27:00 +0200

    Timertomenu

---
+++ MyGameMaker/Script/TimerToMenu.cs
@@ -0,0 +1,29 @@
+﻿using HawkEngine;
+
+public class TimerToMenu : MonoBehaviour
+{
+    public float timer;
+    public float timeToMenu = 15f; // Time in seconds before switching to the main menu
+    bool once = false;
+
+    public override void Awake()
+    {
+
+    }
+
+    public override void Start()
+    {
+
+    }
+
+    public override void Update(float deltaTime)
+    {
+        timer += deltaTime * 1;
+
+        if (timer > timeToMenu && once == false)
+        {
+            SceneManager.LoadScene("MainMenu");
+            once = true;
+        }
+    }
+}

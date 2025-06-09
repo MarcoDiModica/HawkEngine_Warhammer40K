f0d8d1291250c50f0ea8a863c4aaee7a58ccf7bc
Marco Di Modica Elias <marcodimodica@protonmail.com>
2025-06-09 19:26:47 +0200

    more try and catch

--- MyGameMaker/Script/EnemyControllerMelee.cs
+++ MyGameMaker/Script/EnemyControllerMelee.cs
@@ -855,7 +855,7 @@ public class EnemyControllerMelee : EnemyController
     {
         try
         {
-            if (other == null || pc == null || !isLeaping)
+            if (other == null || pc == null || !isLeaping || isDead)
                 return;
 
             if (other.tag == "Player")

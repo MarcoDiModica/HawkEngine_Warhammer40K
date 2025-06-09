0064c7e4a563cb676304de82a525894c29a50fe8
Marco Di Modica Elias <marcodimodica@protonmail.com>
2025-06-09 20:43:00 +0200

    Revert "more try and catch" ahora si
    This reverts commit f0d8d1291250c50f0ea8a863c4aaee7a58ccf7bc.

--- MyGameMaker/Script/EnemyControllerMelee.cs
+++ MyGameMaker/Script/EnemyControllerMelee.cs
@@ -855,8 +855,7 @@ public class EnemyControllerMelee : EnemyController
     {
         try
         {
-            if (other == null || pc == null || !isLeaping)
-                return;
+            if (other == null || pc == null || !isLeaping || isDead) return;
 
             if (other.tag == "Player")
             {

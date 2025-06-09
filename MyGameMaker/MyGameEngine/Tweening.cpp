6da3774feae9bc55833973dde6fdaf942a6f1e6d
Marco Di Modica Elias <marcodimodica@protonmail.com>
2025-06-09 02:13:41 +0200

    fix and more shadows

--- MyGameMaker/MyGameEngine/Tweening.cpp
+++ MyGameMaker/MyGameEngine/Tweening.cpp
@@ -505,7 +505,8 @@ void Tweening::Update(float deltaTime) {
 		case TweenType::POSITION_X:
 		case TweenType::POSITION_Y:
 		case TweenType::POSITION_Z: {
-			if (tween.object && tween.object->GetTransform()) {
+			if (tween.object) {
+				if (!tween.object->GetTransform()) continue;
 				glm::dvec3 currentPosition = CalculatePosition(tween.startPosition, tween.targetPosition, t, tween.mode);
 				tween.object->GetTransform()->SetPosition(currentPosition);
 			}
@@ -528,7 +529,8 @@ void Tweening::Update(float deltaTime) {
 		case TweenType::ROTATION_X:
 		case TweenType::ROTATION_Y:
 		case TweenType::ROTATION_Z: {
-			if (tween.object && tween.object->GetTransform()) {
+			if (tween.object) {
+				if (!tween.object->GetTransform()) continue;
 				glm::dvec3 currentRotation = CalculateRotation(tween.startRotation, tween.targetRotation, t, tween.mode);
 				tween.object->GetTransform()->SetRotation(glm::radians(currentRotation));
 			}
@@ -551,7 +553,8 @@ void Tweening::Update(float deltaTime) {
 		case TweenType::SCALE_X:
 		case TweenType::SCALE_Y:
 		case TweenType::SCALE_Z: {
-			if (tween.object && tween.object->GetTransform()) {
+			if (tween.object) {
+				if (!tween.object->GetTransform()) continue;
 				glm::dvec3 currentScale = CalculateScale(tween.startScale, tween.targetScale, t, tween.mode);
 				tween.object->GetTransform()->SetScale(currentScale);
 			}
